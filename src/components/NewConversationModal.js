c8484d5df67374cc52c0ec0625a35e1a903c0f91
eater <hello@eaterofco.de>
2018-05-02 19:20:35 +0200

    Add Create/Join room buttons

--- src/components/NewConversationModal.js
+++ src/components/NewConversationModal.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {FossilModal} from "./FossilModal";
 import {ContactList} from "./ContactList";
 import {JID} from 'stanza.io'
@@ -9,16 +9,85 @@ class NewConversationModal extends Component {
     super(props);
 
     this.state = {
-      searchValue: false,
+      searchValue: '',
       quickContact: false,
+      quickJoin: false,
+      quickCreate: false,
       isOpen: false,
     };
+
+    this.isMucCache = new Map();
+    this.lookupTimer = false;
   }
 
   updateSearch() {
     this.setState({
-      searchValue: this.search.value,
-      quickContact: /.+@.+/.test(this.search.value),
+      searchValue: this.search.value
+    });
+
+    if (this.lookupTimer) {
+      clearTimeout(this.lookupTimer);
+    }
+
+    if (/.+@.+/.test(this.search.value)) {
+      this.lookupTimer = setTimeout(() => this.checkRoom(this.search.value), 50);
+    }
+  }
+
+  async isMucServer(jid) {
+    let domain = new JID(jid).domain;
+
+    if (!this.isMucCache.has(domain)) {
+      this.isMucCache.set(domain, await this.isMuc(domain));
+    }
+
+    return this.isMucCache.get(domain);
+  }
+
+  async isMuc(jid) {
+    try {
+      const info = await this.props.fossil.client.getDiscoInfo(jid);
+      if (info.discoInfo.features.indexOf('http://jabber.org/protocol/muc') === -1) {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+
+    return true;
+  }
+
+  async checkRoom(val) {
+    if (val !== this.state.searchValue) {
+      return;
+    }
+
+    let isMuc = await this.isMucServer(val);
+
+    if (val !== this.state.searchValue) {
+      return;
+    }
+
+    this.setState({
+      quickJoin: isMuc,
+      quickCreate: false,
+      quickContact: !isMuc,
+    });
+
+    if (!isMuc) {
+      return;
+    }
+
+    let exists = await this.isMuc(val);
+
+    if (val !== this.state.searchValue) {
+      return;
+    }
+
+    this.setState({
+      quickJoin: exists,
+      quickCreate: !exists,
+      quickContact: !isMuc,
     });
   }
 
@@ -33,6 +102,12 @@ class NewConversationModal extends Component {
     this.props.fossil.activateContact(bare);
 
     this.close();
+  }
+
+  onQuickJoin() {
+    let bare = new JID(this.state.searchValue.trim()).bare;
+
+
   }
 
   onContactSelect(jid) {
@@ -55,18 +130,30 @@ class NewConversationModal extends Component {
   }
 
   render() {
+    const bare = new JID((this.state.searchValue || "").trim()).bare;
+
     return <FossilModal className="new-convo-modal" onRequestClose={() => this.close()} isOpen={this.state.isOpen}>
       <div className="quick-search">
         <input ref={a => this.search = a} onKeyUp={() => this.updateSearch()} type="text" placeholder="Search..."/>
       </div>
-      { this.state.quickContact && !this.props.fossil.roster.gotContact(this.state.searchValue.trim())
-        ? <div onClick={() => this.onQuickContact()} className="quick-add">
-          <span className="text">Contact {new JID(this.state.searchValue).bare}</span>
-          <FontAwesomeIcon icon="comment" />
-        </div>
+      {this.state.quickContact && !this.props.fossil.roster.gotContact(bare)
+        ? (
+          <div onClick={() => this.onQuickContact()} className="quick-add">
+            <span className="text">Contact {bare}</span>
+            <FontAwesomeIcon icon="comment"/>
+          </div>
+        )
         : null
       }
-      <ContactList onSelect={jid => this.onContactSelect(jid)} filter={this.state.searchValue} roster={this.props.fossil.roster}/>
+
+      {(this.state.quickJoin || this.state.quickCreate) ? (
+        <div onClick={() => this.onQuickJoin()} className="quick-join">
+          <span className="text">{this.state.quickCreate ? 'Create room': 'Join'} {bare}</span>
+          <FontAwesomeIcon icon="comments"/>
+        </div>
+      ) : null}
+      <ContactList onSelect={jid => this.onContactSelect(jid)} filter={this.state.searchValue}
+                   roster={this.props.fossil.roster}/>
     </FossilModal>
   }
 }
